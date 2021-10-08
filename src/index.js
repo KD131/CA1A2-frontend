84a4e0d389d500cf7ff6469f98c20fac8d6cbdf2
KD131 <71063280+KD131@users.noreply.github.com>
2021-10-09 00:32:43 +0200

    formats to 4 spaces

--- src/index.js
+++ src/index.js
@@ -13,22 +13,22 @@ document.getElementById("all-content").style.display = "block"
 
 /* JS For Exercise-1 below */
 function displayError(err) {
-  let errorElement = document.getElementById("error");
-  if (err.status) {
-    err.fullError.then(e => {
-      errorElement.innerHTML = `${e.code}: ${e.message}`;
-    })
-  }
-  else {
-    errorElement.innerHTML = "Network error";
-  }
+    let errorElement = document.getElementById("error");
+    if (err.status) {
+        err.fullError.then(e => {
+            errorElement.innerHTML = `${e.code}: ${e.message}`;
+        })
+    }
+    else {
+        errorElement.innerHTML = "Network error";
+    }
 }
 
 function getAllPersons() {
-  personFacade.getAll()
-    .then(data => {
-      let dataTableString = data.map(p =>
-        `<tr>
+    personFacade.getAll()
+        .then(data => {
+            let dataTableString = data.map(p =>
+                `<tr>
           <td>${p.id}</td>
           <td>${p.firstName}</td>
           <td>${p.lastName}</td>
@@ -37,10 +37,10 @@ function getAllPersons() {
           <td>${p.address.zip.city}</td>
           <td>${p.address.zip.id}</td>
         </tr>`)
-        .join("");
-      document.getElementById("persons_table").innerHTML = dataTableString;
-    })
-    .catch(displayError)
+                .join("");
+            document.getElementById("persons_table").innerHTML = dataTableString;
+        })
+        .catch(displayError)
 }
 
 /* JS For Exercise-2 below */
@@ -56,31 +56,31 @@ the Period2-week2-day3 Exercises
 */
 
 function hideAllShowOne(idToShow) {
-  document.getElementById("about_html").style = "display:none"
-  document.getElementById("persons_html").style = "display:none"
-  document.getElementById("hobbies_html").style = "display:none"
-  document.getElementById("addresses_html").style = "display:none"
-  document.getElementById(idToShow).style = "display:block"
+    document.getElementById("about_html").style = "display:none"
+    document.getElementById("persons_html").style = "display:none"
+    document.getElementById("hobbies_html").style = "display:none"
+    document.getElementById("addresses_html").style = "display:none"
+    document.getElementById(idToShow).style = "display:block"
 }
 
 function menuItemClicked(evt) {
-  const id = evt.target.id;
-  switch (id) {
-    case "persons_tab":
-      hideAllShowOne("persons_html");
-      getAllPersons();
-      break
-    case "hobbies_tab":
-      hideAllShowOne("hobbies_html");
-      break
-    case "addresses_tab":
-      hideAllShowOne("addresses_html");
-      break
-    default:
-      hideAllShowOne("about_html");
-      break
-  }
-  evt.preventDefault();
+    const id = evt.target.id;
+    switch (id) {
+        case "persons_tab":
+            hideAllShowOne("persons_html");
+            getAllPersons();
+            break
+        case "hobbies_tab":
+            hideAllShowOne("hobbies_html");
+            break
+        case "addresses_tab":
+            hideAllShowOne("addresses_html");
+            break
+        default:
+            hideAllShowOne("about_html");
+            break
+    }
+    evt.preventDefault();
 }
 document.getElementById("menu").onclick = menuItemClicked;
 hideAllShowOne("about_html");
