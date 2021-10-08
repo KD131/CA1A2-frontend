75a308dc1f39aaccc176ca4765376f1191e6af50
KD131 <71063280+KD131@users.noreply.github.com>
2021-10-09 00:49:07 +0200

    adds more methods to personFacade

--- src/personFacade.js
+++ src/personFacade.js
@@ -7,6 +7,43 @@ function getAll() {
         .then(handleHttpErrors);
 }
 
+function getById(id) {
+    return fetch(`${URL}/id/${id}`)
+        .then(handleHttpErrors);
+}
+
+function getByPhone(number) {
+    return fetch(`${URL}/phone/${number}`)
+        .then(handleHttpErrors);
+}
+
+function getByZip(zip) {
+    return fetch(`${URL}/zip/${zip}`)
+        .then(handleHttpErrors);
+}
+
+function getByAddress(id) {
+    return fetch(`${URL}/address/${id}`)
+        .then(handleHttpErrors);
+}
+
+function create(person) {
+    let opts = makeOptions("POST", person);
+    return fetch(`${URL}`, opts)
+}
+     
+function update(person) {
+    let opts = makeOptions("PUT", person);
+    return fetch(`${URL}`, opts)
+        .then(handleHttpErrors);
+}
+
+function deletePerson(id) {
+    let opts = makeOptions("DELETE")
+    return fetch(`${URL}/${id}`)
+        .then(handleHttpErrors);
+}
+
 const personFacade = {
     getAll
 }
